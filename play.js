82973208fb07d51de8b2560a41ccf47415dfab8d
Jesse-D-Miller <jessedonaldmiller@gmail.com>
2025-04-09 08:54:53 -0700

    learning, put a conn.on data function in that i dont understand and now the client outputs a mean mean message when you idle too long

--- play.js
+++ play.js
@@ -3,12 +3,16 @@ const net = require("net");
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: // IP address here,
-    port: // PORT number here,
+    host: "localhost",
+    port: 50541//idk the port number
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+
+  conn.on("data", () => {
+    console.log("you ded cuz you idled");
+  });
 
   return conn;
 };
